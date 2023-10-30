e6ceec6bb0e47cf2a22273e9a6ca08ec0eeaa896
rommuloifrn <roomulo.cavalkant@outlook.com>
2023-10-29 21:23:28 -0300

    mais organizaçao

--- tarefa 3 - ron/server.py
+++ tarefa 3 - ron/server.py
@@ -1,22 +1,33 @@
-import Pyro4
+import Pyro4, random
 
 @Pyro4.expose
-class WordGuessGame:
-    def __init__(self, word):
-        self.palavra = word.lower()
-        self.palavra_montada = ['_'] * len(word)
+class JogoDaForca:
+    def __init__(self):
+        self.palavras_possiveis = ['alessandro', 'demostenes', 'jorgiano', 'gracon', 'silvia']
+        self.palavra = self.palavras_possiveis[random.randrange(0, len(self.palavras_possiveis))]
+        self.palavra_montada = ['_'] * len(self.palavra)
         self.tentativas_restantes = 5
 
-    def vitoria(self):
+    def sorteia_palavra(self):
+        numero = random.randrange(0, len(self.palavras_possiveis))
+        self.palavra = self.palavras_possiveis[numero]
+
+    def fim(self, vitoria):
+        palavra_antiga = self.palavra
+        self.sorteia_palavra()
         self.palavra_montada = ['_'] * len(self.palavra) # reseta as letras adivinhadas
         self.tentativas_restantes = 5 # reseta as tentativas restantes
-        return "Parabéns! Você adivinhou a palavra!"
+        
+        if vitoria:
+            return "Parabéns! Você adivinhou a palavra!"
+        else:
+            return f"Você perdeu! A palavra era: {palavra_antiga}"
 
     def adivinhar(self, palpite):
         palpite = palpite.lower()
         
         if palpite == self.palavra:
-            return self.vitoria()
+            return self.fim(True)
 
         # caso o palpite esteja na palavra e seja menor que 2
         if (palpite in self.palavra) and (len(palpite)<2):
@@ -27,24 +38,25 @@ class WordGuessGame:
             # notifica o acerto da palavra ou da letra
             if (''.join(self.palavra_montada) == self.palavra):
                 self.palavra_montada = ['_'] * len(self.palavra) # reseta as letras adivinhadas
-                return self.vitoria()
-            else: return "Você acertou a letra: " + ' '+palpite + " DEBUG - guessed word = " + ''.join(self.palavra_montada)
-        #caso o palpite nem esteja na palavra e sem seja a palavra
+                return self.fim(True)
+            else: return "Você acertou a letra: " + ''.join(self.palavra_montada)
+        # caso o palpite nem esteja na palavra e sem seja a palavra
         else:
             self.tentativas_restantes -= 1
             if self.tentativas_restantes == 0:
-                return f"Você perdeu! A palavra era: {self.palavra}"
+                return self.fim(False)
             return f"Palpite incorreto. Tentativas restantes: {self.tentativas_restantes}"
 
+
+
 def main():
-    # Defina a palavra a ser adivinhada aqui
-    palavra = "python"  
-    game = WordGuessGame(palavra)
+    # Defina a palavra a ser adivinhada aqui  
+    jogo = JogoDaForca()
 
     # simplificação para os comandos mais comuns
     Pyro4.Daemon.serveSimple(
         {
-            game: "jogo_da_forca",
+            jogo: "jogo_da_forca",
         },
         ns = True # nameserver
     )
