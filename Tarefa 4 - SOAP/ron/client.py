8e1f475a648d9427ecc42e047d423a437466fc83
Rômulo da Silva Cavalcanti <20221014040015@ifrn.local>
2023-11-16 15:30:11 -0300

    apagados dotnet

---
+++ Tarefa 4 - SOAP/ron/client.py
@@ -0,0 +1,5 @@
+import zeep
+
+wsdl = 'http://www.soapclient.com/xml/soapresponder.wsdl'
+client = zeep.Client(wsdl=wsdl)
+print(client.service.Method1("ah meu deus como eh bom ser da botinha", "e tomar guarana"))
