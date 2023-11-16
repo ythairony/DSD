6a9fa059a0cf230195cd76c57531f21c3a072b0a
YURI THAIRONY FEITOSA DE OLIVEIRA <20222014040015@ifrn.local>
2023-11-16 09:32:30 -0300

    Iniciando servidor em JS

---
+++ Tarefa 4 - SOAP/ServidorEmJS/server.js
@@ -0,0 +1,22 @@
+const soap = require("soap")
+const http = require("http")
+
+const WSDL_PATH = "/QAjudaFinancas.wsdl"
+const WSDL_DEFINITION = require("fs").readFileSync(WSDL_PATH, "utf8")
+
+const servico = {
+    QAjuda_servico: {
+        porta: {
+            
+        }
+    }
+}
+
+const servidor = http.createServer(function(req, res) {
+    res.end("404: Not found " + req.url)
+})
+
+servidor.listen(8000)
+SOAP.listen(servidor, "/QAjuda", servico, WSDL_DEFINITION) // temporário...
+
+console.log("QAjuda Finanças rodando...")
