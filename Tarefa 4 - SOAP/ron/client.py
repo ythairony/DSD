9448776d036279dd4570da28a059e134262cef4a
Rômulo da Silva Cavalcanti <20221014040015@ifrn.local>
2023-11-16 15:57:15 -0300

    ta indo

--- Tarefa 4 - SOAP/ron/client.py
+++ Tarefa 4 - SOAP/ron/client.py
@@ -1,5 +1,9 @@
 import zeep
 
-wsdl = 'http://www.soapclient.com/xml/soapresponder.wsdl'
+#wsdl = 'http://www.soapclient.com/xml/soapresponder.wsdl'
+#client = zeep.Client(wsdl=wsdl)
+#print(client.service.Method1("ah meu deus como eh bom ser da botinha", "e tomar guarana"))
+
+wsdl = 'QAjudaFinancas.wsdl'
 client = zeep.Client(wsdl=wsdl)
-print(client.service.Method1("ah meu deus como eh bom ser da botinha", "e tomar guarana"))
+print(client.service.vezesDois(3))
